392e2c4e7b9b9ed8e8d6423a00d33d45e5943777
Gerlinde1997 <gerlinde.klarinet@gmail.com>
2022-01-24 15:32:30 +0100

    complete navigation tutorial

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -26,7 +26,7 @@ class TitleFragment : Fragment() {
             container, false)
 
         binding.playButton.setOnClickListener{ view : View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
 
         binding.rulesButton.setOnClickListener{ view: View ->
